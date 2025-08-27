28162db268d3aee87c6db0f185224b3da3df4618
Vyacheslav Razlada <vrazlada@mail.ru>
2025-08-27 21:56:47 +0300

    feat: add stats component & svg sprite loader

---
+++ src/common/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button" type="button">
+    <span class="button__icon">
+      <slot name="button-icon" />
+    </span>
+    <slot>Сохранить</slot>
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-xs);
+  padding: var(--spacing-s);
+
+  color: var(--color-light);
+  font-size: var(--font-size-base);
+  font-weight: var(--font-weight-semibold);
+  background-image: var(--gradient-base);
+  border-radius: var(--border-radius-medium);
+  transition: background-image 0.3s ease;
+  cursor: pointer;
+
+  @media (hover: hover) {
+    &:hover {
+      background-image: var(--gradient-hover);
+    }
+  }
+
+  @media (hover: none) {
+    &:active {
+      background-image: var(--gradient-hover);
+    }
+  }
+}
+</style>
---
+++ src/modules/stats/StatsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="stats">
+    <h2 class="visually-hidden">Статистика</h2>
+    <ul class="stats-list">
+      <StatsItem v-for="{ title, value } in items" :key="title" :title :value />
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import StatsItem from "./StatsItem.vue";
+
+const items = [
+  {
+    title: "Влажность",
+    value: "90%",
+  },
+  {
+    title: "Осадки",
+    value: "20 мм",
+  },
+
+  {
+    title: "Ветер",
+    value: "10 м/с",
+  },
+];
+</script>
+
+<style lang="scss" scoped>
+.stats-list {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-m);
+}
+</style>
